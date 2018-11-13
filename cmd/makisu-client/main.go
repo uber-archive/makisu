99cd1c67c1a9b27d0ae56bb2a0149c617375b07a
Antoine Pourchet <pourchet@uber.com>
2018-11-13 15:30:03 -0800

    Revert "Simpler dockerfiles"
    This reverts commit 7d8c79fa171cc6126fe7482cd4535800e95d5a6b.

--- cmd/makisu-client/main.go
+++ cmd/makisu-client/main.go
@@ -5,11 +5,14 @@ import (
 	"os"
 
 	"github.com/uber/makisu/cli"
+	"github.com/uber/makisu/lib/utils"
 
 	"github.com/apourchet/commander"
 )
 
 func main() {
+	log.Printf("Starting makisu (version=%s)", utils.BuildHash)
+
 	application := cli.NewClientApplication()
 	cmd := commander.New()
 	if err := cmd.RunCLI(application, os.Args[1:]); err != nil {
