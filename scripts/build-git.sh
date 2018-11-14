54919296cf9e7c80d1cb4e720d211cfe89addd3f
Antoine Pourchet <antoine.pourchet@gmail.com>
2018-11-14 13:23:14 -0800

    Added example with github token for private repo building (#9)

--- scripts/build-git.sh
+++ scripts/build-git.sh
@@ -4,6 +4,10 @@
 repo=$1
 
 set -ex
+if [ -f "/makisu-secrets/github-token/github_token" ]; then 
+    repo="$(cat /makisu-secrets/github-token/github_token)@$repo"
+fi
+repo="https://$repo"
 git clone "$repo" /tmp/repo
 
 cd /tmp/repo
