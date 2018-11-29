2b7dd81574ac63e96d406cea338f9c1c48043cb2
Antoine Pourchet <antoine.pourchet@gmail.com>
2018-11-29 13:41:16 -0800

    Fixed gocyclo for parser code (#92)

--- lib/parser/dockerfile/directive.go
+++ lib/parser/dockerfile/directive.go
@@ -19,6 +19,46 @@ type Directive interface {
 	update(*parsingState) error
 }
 
+type directiveConstructor func(*baseDirective, *parsingState) (Directive, error)
+
+var directiveConstructors = map[string]directiveConstructor{
+	"add": func(base *baseDirective, state *parsingState) (Directive, error) { return newAddDirective(base, state) },
+	"arg": func(base *baseDirective, state *parsingState) (Directive, error) { return newArgDirective(base, state) },
+	"cmd": func(base *baseDirective, state *parsingState) (Directive, error) { return newCmdDirective(base, state) },
+	"copy": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newCopyDirective(base, state)
+	},
+	"entrypoint": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newEntrypointDirective(base, state)
+	},
+	"env": func(base *baseDirective, state *parsingState) (Directive, error) { return newEnvDirective(base, state) },
+	"expose": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newExposeDirective(base, state)
+	},
+	"from": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newFromDirective(base, state)
+	},
+	"label": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newLabelDirective(base, state)
+	},
+	"maintainer": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newMaintainerDirective(base, state)
+	},
+	"run": func(base *baseDirective, state *parsingState) (Directive, error) { return newRunDirective(base, state) },
+	"stopsignal": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newStopsignalDirective(base, state)
+	},
+	"user": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newUserDirective(base, state)
+	},
+	"volume": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newVolumeDirective(base, state)
+	},
+	"workdir": func(base *baseDirective, state *parsingState) (Directive, error) {
+		return newWorkdirDirective(base, state)
+	},
+}
+
 // newDirective initializes a directive from a line of a Dockerfile and
 // the current build state.
 // This function also serves to define the variable replacement behavior
@@ -27,43 +67,13 @@ func newDirective(line string, state *parsingState) (Directive, error) {
 	base, err := newBaseDirective(line)
 	if err != nil {
 		return nil, err
-	}
-	if base == nil {
+	} else if base == nil {
 		return nil, nil
 	}
 
-	switch base.t {
-	case "add":
-		return newAddDirective(base, state)
-	case "arg":
-		return newArgDirective(base, state)
-	case "cmd":
-		return newCmdDirective(base, state)
-	case "copy":
-		return newCopyDirective(base, state)
-	case "entrypoint":
-		return newEntrypointDirective(base, state)
-	case "env":
-		return newEnvDirective(base, state)
-	case "expose":
-		return newExposeDirective(base, state)
-	case "from":
-		return newFromDirective(base, state)
-	case "label":
-		return newLabelDirective(base, state)
-	case "maintainer":
-		return newMaintainerDirective(base, state)
-	case "run":
-		return newRunDirective(base, state)
-	case "stopsignal":
-		return newStopsignalDirective(base, state)
-	case "user":
-		return newUserDirective(base, state)
-	case "volume":
-		return newVolumeDirective(base, state)
-	case "workdir":
-		return newWorkdirDirective(base, state)
-	default:
+	cons, found := directiveConstructors[base.t]
+	if !found {
 		return nil, base.err(errUnsupportedDirective)
 	}
+	return cons(base, state)
 }
