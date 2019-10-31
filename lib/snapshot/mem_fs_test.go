b277fa013cce25f3cb952e57cf885f3dce209c1f
Antoine Pourchet <pourchet@uber.com>
2019-10-31 11:03:58 -0700

    Fixed unit tests

--- lib/snapshot/mem_fs_test.go
+++ lib/snapshot/mem_fs_test.go
@@ -33,7 +33,7 @@ func TestUntarFromPath(t *testing.T) {
 
 	tmpBase, err := ioutil.TempDir("/tmp", "makisu-test")
 	require.NoError(err)
-	defer os.RemoveAll(tmpBase)
+	// defer os.RemoveAll(tmpBase)
 
 	tmpRoot, err := ioutil.TempDir(tmpBase, "root")
 	require.NoError(err)
@@ -89,9 +89,9 @@ func TestUntarFromPath(t *testing.T) {
 	require.NoError(err)
 	require.False(fi.IsDir())
 
-	contents, err = ioutil.ReadFile(filepath.Join(tmpRoot, "mydir"))
+	target, err := os.Readlink(filepath.Join(tmpRoot, "mydir"))
 	require.NoError(err)
-	require.Equal([]byte("TARGET"), contents)
+	require.Equal("/target.txt", target)
 
 	require.Equal(7, fs.layers[len(fs.layers)-1].count())
 
