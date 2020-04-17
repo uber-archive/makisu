70cabaa5b56dad44be5b2e08ab6c20a892efdf62
jockeych <xiaoweic@uber.com>
2020-04-17 11:06:03 -0700

    fix the parent directory ownership when using COPY --archive (#317)
    * fix the permssion of given directory
    
    * address comments and separate the logic of maintain ownership from mkdirAll func
    
    * fix typo

--- lib/fileio/copy.go
+++ lib/fileio/copy.go
@@ -104,7 +104,7 @@ func (c copier) CopyDir(source, target string, uid, gid int) error {
 	if err := mkdirAll(target, os.ModePerm, uid, gid, false); err != nil {
 		return fmt.Errorf("mkdir all %s: %s", target, err)
 	}
-	// Recursively copy directories and files.
+	// Recursively copy contents of source directory.
 	return c.copyDirContents(source, target, target, uid, gid, false)
 }
 
@@ -115,11 +115,26 @@ func (c copier) CopyDirPreserveOwner(source, target string) error {
 		log.Infof("* Ignoring copy of directory %s because it is blacklisted", source)
 		return nil
 	}
-	// Make target parent directories (uid and gid will be computed from the sources one).
-	if err := mkdirAll(target, os.ModePerm, 0, 0, true); err != nil {
-		return fmt.Errorf("mkdir all %s: %s", target, err)
+	// Copy the parent directory of target.
+	targetParentDir := filepath.Dir(target)
+	if err := mkdirAll(targetParentDir, os.ModePerm, 0, 0, true); err != nil {
+		return fmt.Errorf("mkdir parent dir %s: %s", targetParentDir, err)
 	}
-	// Recursively copy directories and files.
+	// If the source dir is a symlink, makisu would try to copy the contents rather than a link.
+	fi, _ := os.Stat(source)
+	sourceUid, sourceGid := fileOwners(fi)
+	if _, err := os.Lstat(target); err != nil {
+		if !os.IsNotExist(err) {
+			return fmt.Errorf("stat %s: %s", target, err)
+		} else if err := os.Mkdir(target, os.ModePerm); err != nil {
+			return fmt.Errorf("mkdir %s: %s", target, err)
+		}
+	}
+	// Preserve the source dir ownership.
+	if err := os.Chown(target, sourceUid, sourceGid); err != nil {
+		return fmt.Errorf("chown %s: %s", target, err)
+	}
+	// Recursively copy contents of source directory.
 	return c.copyDirContents(source, target, target, 0, 0, true)
 }
 
@@ -289,7 +304,7 @@ func (c copier) copyDir(src, dst string, uid, gid int, preserveOwner bool) error
 
 // mkdirAll performs the same operation as os.MkdirAll, but also sets the given
 // permissions & owners on all created directories.
-func mkdirAll(dst string, mode os.FileMode, uid, gid int, preserveOwner bool) error {
+func mkdirAll(dst string, mode os.FileMode, uid, gid int, preserveParentOwner bool) error {
 	if dst == "" {
 		return errors.New("empty target directory")
 	}
@@ -300,7 +315,6 @@ func mkdirAll(dst string, mode os.FileMode, uid, gid int, preserveOwner bool) er
 
 	split := strings.Split(abs, "/")
 	split[0] = "/"
-
 	var prevDir string
 	for _, dir := range split {
 		absDir := filepath.Join(prevDir, dir)
@@ -312,8 +326,8 @@ func mkdirAll(dst string, mode os.FileMode, uid, gid int, preserveOwner bool) er
 			}
 
 			// Update file info
-			fi, _ = os.Lstat(absDir)
-			if preserveOwner {
+			fi, _ = os.Lstat(prevDir)
+			if preserveParentOwner {
 				uid, gid = fileOwners(fi)
 			}
 			if err := os.Chown(absDir, uid, gid); err != nil {
@@ -322,6 +336,7 @@ func mkdirAll(dst string, mode os.FileMode, uid, gid int, preserveOwner bool) er
 		}
 		prevDir = absDir
 	}
+
 	return nil
 }
 
